383c4de937fe56d78b8d5d98a1eee2cb9d891cbf
Fabricio Veronez <fabricioveronez@gmail.com>
2021-03-05 21:42:56 -0300

    app

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
 import flask
 from flask import request, jsonify
+from flask_restplus import Resource, Api
 
 app = flask.Flask(__name__)
-# app.config["DEBUG"] = True
+api = Api(app)
 
-@app.route('/', methods=['GET'])
+def celsius_fahrenheit(temperatura=None):
+    fahrenheit = (temperatura * 9 / 5) + 32
+    return fahrenheit 
+
+def fahrenheit_celsius(temperatura=None):
+    celsius = (temperatura - 32) * 5 / 9
+    return celsius
+
+@api.route('/', methods=['GET'])
 def home():
     return "Api de Conversão em Python-v3"
 
+@api.route('/celsius/<int:temperatura>fahrenheit')
+class CelsiusFahrenheit(Resource):
+    def get(self, temperatura):
+        return jsonify(celsius_fahrenheit(temperatura))
 
-@app.route('/celsius/<int:temperatura>/fahrenheit', methods=['GET'])
-def celsius_fahrenheit(temperatura=None):
-    fahrenheit = (temperatura * 9 / 5) + 32
-    return jsonify({ 'fahrenheit': fahrenheit })
+@api.route('/fahrenheit/<int:temperatura>/celsius')
+class FahrenheitCelsius(Resource):
+    def get(self, temperatura):
+        return jsonify(fahrenheit_celsius(temperatura))
 
-@app.route('/fahrenheit/<int:temperatura>/celsius', methods=['GET'])
-def fahrenheit_celsius(temperatura=None):
-    celsius = (temperatura - 32) * 5 / 9
-    return jsonify({ 'celsius': celsius })
 
 if __name__ == '__main__':
     app.run()
