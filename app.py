5eafc69fb539e652919ddd68fcbd7ed752b61f51
Fabricio Veronez <fabricioveronez@gmail.com>
2021-03-06 20:58:46 -0300

    Adicionando namespace

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import flask
-from flask import request, jsonify
 from flask_restplus import Resource, Api
 
 app = flask.Flask(__name__)
-api = Api(app)
+api = Api(app, '1.0', 'Api de conversão de temperatura', '')
+
 
 def celsius_fahrenheit(temperatura=None):
     fahrenheit = (temperatura * 9 / 5) + 32
@@ -13,20 +13,17 @@ def fahrenheit_celsius(temperatura=None):
     celsius = (temperatura - 32) * 5 / 9
     return celsius
 
-@api.route('/', methods=['GET'])
-def home():
-    return "Api de Conversão em Python-v3"
+ns = api.namespace('temperatura', description='Conversões de temperatura.')
 
-@api.route('/celsius/<int:temperatura>fahrenheit')
+@ns.route('/celsius/<int:temperatura>fahrenheit')
 class CelsiusFahrenheit(Resource):
     def get(self, temperatura):
-        return jsonify(celsius_fahrenheit(temperatura))
+        return celsius_fahrenheit(temperatura)
 
-@api.route('/fahrenheit/<int:temperatura>/celsius')
+@ns.route('/fahrenheit/<int:temperatura>/celsius')
 class FahrenheitCelsius(Resource):
     def get(self, temperatura):
-        return jsonify(fahrenheit_celsius(temperatura))
-
+        return fahrenheit_celsius(temperatura)
 
 if __name__ == '__main__':
     app.run()
